3501bbe98a80dbdac8ce28aeed53cfb48be0dd1d
William Oliveira <w.oliveira542@gmail.com>
2017-04-23 13:42:53 -0300

    Add htmlFor on SearchEngine

--- src/components/SearchEngine.js
+++ src/components/SearchEngine.js
@@ -4,8 +4,8 @@ const SearchEngine = (props) => {
     return(
         <form  className="search-engine" onSubmit={ props.handleForm }>
         <h2>Consultar</h2>
-        <label>CEP: </label>
-        <input type="number" placeholder="02050-010" required autoFocus name="postalCode"/>
+        <label htmlFor="postalCode">CEP: </label>
+        <input type="number" placeholder="02050-010" required autoFocus name="postalCode" id="postalCode"/>
         <button>Buscar</button>
         </form>
     )
